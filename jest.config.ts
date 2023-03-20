f11f89ccdcda76fe80cfd1bcd783059558dae4a0
natan22gt <natsuaz27@gmail.com>
2023-03-19 23:11:36 -0300

    chore: jest and config files

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+export default {
+  roots: ['<rootDir>/src'],
+
+  coverageProvider: 'v8',
+
+  collectCoverage: true,
+
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+
+  coverageDirectory: 'coverage',
+
+  testEnvironment: 'node',
+
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)']
+}
